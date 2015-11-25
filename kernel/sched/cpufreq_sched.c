63eb1aa58e6195eb1113515eb7f47021e19df2d3
Steve Muckle <smuckle@linaro.org>
2015-11-25 15:59:25 -0800

    WIP: sched/cpufreq_sched: add trace events
    Trace events will aid in debugging, profiling and tuning.
    
    BUG=chrome-os-partner:44828
    TEST=Boot kernel on Oak.
    
    Change-Id: I714e1875a6509e6da4308fa2e76a55ad107b35a5
    Signed-off-by: Steve Muckle <smuckle@linaro.org>

--- kernel/sched/cpufreq_sched.c
+++ kernel/sched/cpufreq_sched.c
@@ -14,6 +14,9 @@
 #include <linux/delay.h>
 #include <linux/string.h>
 
+#define CREATE_TRACE_POINTS
+#include <trace/events/cpufreq_sched.h>
+
 #include "sched.h"
 
 #define THROTTLE_NSEC		50000000 /* 50ms default */
@@ -77,12 +80,13 @@ static bool finish_last_request(struct gov_data *gd)
     while (1) {
 	int usec_left = ktime_to_ns(ktime_sub(gd->throttle, now));
 
-	usec_left /= NSEC_PER_USEC;
-	usleep_range(usec_left, usec_left + 100);
-	now = ktime_get();
-	if (ktime_after(now, gd->throttle))
-	    return true;
-    }
+		usec_left /= NSEC_PER_USEC;
+		trace_cpufreq_sched_throttled(usec_left);
+		usleep_range(usec_left, usec_left + 100);
+		now = ktime_get();
+		if (ktime_after(now, gd->throttle))
+			return true;
+	}
 }
 
 /*
@@ -205,8 +209,11 @@ static void update_fdomain_capacity_request(int cpu)
 	goto out;
     freq_new = policy->freq_table[index_new].frequency;
 
-    if (freq_new == gd->requested_freq)
-	goto out;
+	trace_cpufreq_sched_request_opp(cpu, capacity, freq_new,
+					gd->requested_freq);
+
+	if (freq_new == gd->requested_freq)
+		goto out;
 
     gd->requested_freq = freq_new;
 
@@ -241,9 +248,11 @@ void update_cpu_capacity_request(int cpu, bool request)
     if (new_capacity == scr->total)
 	return;
 
-    scr->total = new_capacity;
-    if (request)
-	update_fdomain_capacity_request(cpu);
+	trace_cpufreq_sched_update_capacity(cpu, request, scr, new_capacity);
+
+	scr->total = new_capacity;
+	if (request)
+		update_fdomain_capacity_request(cpu);
 }
 
 static inline void set_sched_freq(void)
