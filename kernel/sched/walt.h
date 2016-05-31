48b9b53ff1de06120eaa629fd2f9e81b5849efe6
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2016-05-31 09:08:38 -0700

    sched: Introduce Window Assisted Load Tracking (WALT)
    Use a window based view of time in order to track task
    demand and CPU utilization in the scheduler.
    
    Change-Id: I21408236836625d4e7d7de1843d20ed5ff36c708
    
    Signed-off-by: Srinath Sridharan <srinathsr@google.com>
    
    sched/walt: fix build issue
    
    sysctl options were outside CONFIG_SCHED_WALT and this was causing build
    to fail when SCHED_WALT=N.
    
    Fix it. Probably to be squashed into 0192-sched-Introduce-Window-Assisted-
    Load-Tracking-WALT.patch
    
    Signed-off-by: Juri Lelli <juri.lelli@arm.com>

--- kernel/sched/walt.h
+++ kernel/sched/walt.h
@@ -17,13 +17,13 @@
 #ifdef CONFIG_SCHED_WALT
 
 void walt_update_task_ravg(struct task_struct *p, struct rq *rq, int event,
-		u64 wallclock, u64 irqtime);
+	u64 wallclock, u64 irqtime);
 void walt_inc_cumulative_runnable_avg(struct rq *rq, struct task_struct *p);
 void walt_dec_cumulative_runnable_avg(struct rq *rq, struct task_struct *p);
 void walt_inc_cfs_cumulative_runnable_avg(struct cfs_rq *rq,
-		struct task_struct *p);
+	struct task_struct *p);
 void walt_dec_cfs_cumulative_runnable_avg(struct cfs_rq *rq,
-		struct task_struct *p);
+	struct task_struct *p);
 void walt_fixup_busy_time(struct task_struct *p, int new_cpu);
 void walt_init_new_task_load(struct task_struct *p);
 void walt_mark_task_starting(struct task_struct *p);
@@ -31,22 +31,17 @@ void walt_set_window_start(struct rq *rq);
 void walt_migrate_sync_cpu(int cpu);
 void walt_init_cpu_efficiency(void);
 u64 walt_ktime_clock(void);
-void walt_account_irqtime(int cpu, struct task_struct *curr, u64 delta,
-                                  u64 wallclock);
-
-u64 walt_irqload(int cpu);
-int walt_cpu_high_irqload(int cpu);
 
 #else /* CONFIG_SCHED_WALT */
 
 static inline void walt_update_task_ravg(struct task_struct *p, struct rq *rq,
-		int event, u64 wallclock, u64 irqtime) { }
+	int event, u64 wallclock, u64 irqtime) { }
 static inline void walt_inc_cumulative_runnable_avg(struct rq *rq, struct task_struct *p) { }
 static inline void walt_dec_cumulative_runnable_avg(struct rq *rq, struct task_struct *p) { }
 static inline void walt_inc_cfs_cumulative_runnable_avg(struct cfs_rq *rq,
-		struct task_struct *p) { }
+	struct task_struct *p) { }
 static inline void walt_dec_cfs_cumulative_runnable_avg(struct cfs_rq *rq,
-		struct task_struct *p) { }
+	struct task_struct *p) { }
 static inline void walt_fixup_busy_time(struct task_struct *p, int new_cpu) { }
 static inline void walt_init_new_task_load(struct task_struct *p) { }
 static inline void walt_mark_task_starting(struct task_struct *p) { }
