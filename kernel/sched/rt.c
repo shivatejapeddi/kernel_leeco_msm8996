50d18d2fd063dc94df733ad5f58eb0a0003aee03
John Dias <joaodias@google.com>
2016-09-15 08:52:27 -0700

    sched: avoid scheduling RT threads on cores currently handling softirqs
    Bug: 31501544
    Change-Id: I99dd7aaa12c11270b28dbabea484bcc8fb8ba0c1

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1449,6 +1449,19 @@ task_may_not_preempt(struct task_struct *task, int cpu)
 		 task_pc & SOFTIRQ_MASK));
 }
 
+/*
+ * Return whether the task on the given cpu is currently non-preemptible
+ * while handling a softirq or is likely to block preemptions soon because
+ * it is a ksoftirq thread.
+ */
+bool
+task_may_not_preempt(struct task_struct *task, int cpu)
+{
+	struct task_struct *cpu_ksoftirqd = per_cpu(ksoftirqd, cpu);
+	return (task_thread_info(task)->preempt_count & SOFTIRQ_MASK) ||
+	       task == cpu_ksoftirqd;
+}
+
 static int
 select_task_rq_rt(struct task_struct *p, int cpu, int sd_flag, int flags,
 		  int sibling_count_hint)
