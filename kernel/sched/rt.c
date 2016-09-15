3ca63f384517c4cf3ba687a66e590f7060484f5d
John Dias <joaodias@google.com>
2016-09-15 08:52:27 -0700

    sched: avoid scheduling RT threads on cores currently handling softirqs
    Bug: 31501544
    Change-Id: I99dd7aaa12c11270b28dbabea484bcc8fb8ba0c1

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -1384,6 +1384,19 @@ static void yield_task_rt(struct rq *rq)
 #ifdef CONFIG_SMP
 static int find_lowest_rq(struct task_struct *task);
 
+/*
+ * Return whether the task on the given cpu is currently non-preemptible
+ * while handling a softirq or is likely to block preemptions soon because
+ * it is a ksoftirq thread.
+ */
+bool
+task_may_not_preempt(struct task_struct *task, int cpu)
+{
+	struct task_struct *cpu_ksoftirqd = per_cpu(ksoftirqd, cpu);
+	return (task_thread_info(task)->preempt_count & SOFTIRQ_MASK) ||
+	       task == cpu_ksoftirqd;
+}
+
 static int
 select_task_rq_rt(struct task_struct *p, int cpu, int sd_flag, int flags)
 {
@@ -1434,10 +1447,10 @@ select_task_rq_rt(struct task_struct *p, int cpu, int sd_flag, int flags)
 		 * even if it's running a higher-prio task.
  		 * Otherwise: Possible race. Don't bother moving it if the
 		 * destination CPU is not running a lower priority task.
-		 */
-                if (target != -1 &&
+ 		 */
+		if (target != -1 &&
 		    (may_not_preempt ||
-                    p->prio < cpu_rq(target)->rt.highest_prio.curr)
+		     p->prio < cpu_rq(target)->rt.highest_prio.curr))
 			cpu = target;
 	}
 	rcu_read_unlock();
