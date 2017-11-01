42b2af21d3e23cfec89aa3d8117969d8e2d14980
1nfrag <sil3ntcll@gmail.com>
2017-11-01 21:56:40 +0800

    Actuator restore stock values
    Change-Id: I826d5cacb71cf78d503124406a05aea18c69fbe2

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -316,7 +316,7 @@ static int msm_actuator_bivcm_handle_i2c_ops(
 			else if(strcmp(a_ctrl->project_name,"x2")==0)
 			{
 				out[0] = i2c_byte1;
-	        			out[1] = 0xd0;
+	        		out[1] = 0x90;
 				out[2] = 0x0;
 				out[3] = i2c_byte2 / 256;
 				out[4] = i2c_byte2 % 256;
