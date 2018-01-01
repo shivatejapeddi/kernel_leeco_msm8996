b5bf7dee9812f0896dff43c93c45e148adb98f04
1nfrag <sil3ntcll@gmail.com>
2018-01-01 09:42:25 +0800

    Remove USB_FLOAT
    Change-Id: Ic9a2d8338f1e680859ab5747af7aa221912667e7

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -46,7 +46,7 @@ static ssize_t power_supply_show_property(struct device *dev,
 	static const char * const type_text[] = {
 		"Unknown", "Battery", "UPS", "Mains", "USB",
 		"USB_DCP", "USB_CDP", "USB_ACA",
-		"USB_HVDCP", "USB_HVDCP_3", "USB_FLOAT", "USB_PD", "USB_LE_PD", "Wireless", "BMS",
+		"USB_HVDCP", "USB_HVDCP_3", "USB_PD", "USB_LE_PD", "Wireless", "BMS",
 		"USB_Parallel", "Wipower", "TYPEC", "TYPEC_UFP", "TYPEC_DFP"
 	};
 	static char *status_text[] = {
