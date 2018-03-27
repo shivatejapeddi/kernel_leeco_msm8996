bae51b6ef39ca7be157b41fcf328881f09d4be3a
Martichou <martichou.andre@gmail.com>
2018-03-27 16:08:40 +0000

    sched: Revert some HMP commits for a cleaner EAS implementation (squashed)
    Revert "sched: Add a knob to disable the core_ctl"
    
    This reverts commit e3e179b299a72a6d52cbd676c840c0883805c677.
    
    Revert "trace: Move core control trace events to scheduler"
    
    This reverts commit bd33ce82c9c91f694034735d81d215598f7b0780.
    
    Revert "core_ctrl: Move core control into kernel"
    
    This reverts commit 6fa527a79541c31bb717d5fc60f89b7b14b25b30.
    
    Revert "sched: Fix deadlock between cpu hotplug and upmigrate change"
    
    This reverts commit 10693dee4bc3df701c64b444e829479e5735d5e0.
    
    Revert "sched: drop related_thread_group_lock before calling call_rcu()"
    
    This reverts commit 2461d66ac2ab4f5d869c0f497223699b3e06c930.
    
    Revert "sched: qhmp_core: Fix possible hotplug race in  set_cpus_allowed_ptr"
    
    This reverts commit cbc9c4624801779f6e6a663c70489ed5d85e3b9c.
    
    Revert "sched: panic on corrupted stack end"
    
    This reverts commit 6c7fbc2c02997c591ce0df35c27144103791113b.
    
    Revert "sched: Fix cpu_active_mask/cpu_online_mask race"
    
    This reverts commit c9709600336dba7140660b65394fdc8bbd0dbb2d.
    
    Revert "sched: Introduce sched_freq_aggregate_threshold tunable"
    
    This reverts commit 606e7bca8bd4bc41cf4c7c1cd2d30b904bc136df.

--- kernel/sched/qhmp_core.c
+++ kernel/sched/qhmp_core.c
@@ -4532,8 +4532,7 @@ static noinline void __schedule_bug(struct task_struct *prev)
 static inline void schedule_debug(struct task_struct *prev)
 {
 #ifdef CONFIG_SCHED_STACK_END_CHECK
-	if (unlikely(task_stack_end_corrupted(prev)))
-		panic("corrupted stack end detected inside scheduler\n");
+	BUG_ON(unlikely(task_stack_end_corrupted(prev)));
 #endif
 	/*
 	 * Test if we are atomic. Since do_exit() needs to call into
@@ -6607,8 +6606,7 @@ int set_cpus_allowed_ptr(struct task_struct *p, const struct cpumask *new_mask)
 	if (cpumask_equal(&p->cpus_allowed, new_mask))
 		goto out;
 
-	dest_cpu = cpumask_any_and(cpu_active_mask, new_mask);
-	if (dest_cpu >= nr_cpu_ids) {
+	if (!cpumask_intersects(new_mask, cpu_active_mask)) {
 		ret = -EINVAL;
 		goto out;
 	}
@@ -6619,6 +6617,7 @@ int set_cpus_allowed_ptr(struct task_struct *p, const struct cpumask *new_mask)
 	if (cpumask_test_cpu(task_cpu(p), new_mask))
 		goto out;
 
+	dest_cpu = cpumask_any_and(cpu_active_mask, new_mask);
 	if (task_running(rq, p) || p->state == TASK_WAKING) {
 		struct migration_arg arg = { p, dest_cpu };
 		/* Need help from migration thread: drop lock and wait. */
@@ -7167,14 +7166,6 @@ static int sched_cpu_active(struct notifier_block *nfb,
 	case CPU_STARTING:
 		set_cpu_rq_start_time();
 		return NOTIFY_OK;
-	case CPU_ONLINE:
-		/*
-		 * At this point a starting CPU has marked itself as online via
-		 * set_cpu_online(). But it might not yet have marked itself
-		 * as active, which is essential from here on.
-		 *
-		 * Thus, fall-through and help the starting CPU along.
-		 */
 	case CPU_DOWN_FAILED:
 		set_cpu_active((long)hcpu, true);
 		return NOTIFY_OK;
