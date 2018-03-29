a8dc42f2030bc951bb238049393adb84261e7def
Minchan Kim <minchan@kernel.org>
2018-03-29 06:54:00 -0700

    binder: change down_write to down_read
    binder_update_page_range needs down_write of mmap_sem because
    vm_insert_page need to change vma->vm_flags to VM_MIXEDMAP unless
    it is set. However, when I profile binder working, it seems
    every binder buffers should be mapped in advance by binder_mmap.
    It means we could set VM_MIXEDMAP in binder_mmap time which is
    already hold a mmap_sem as down_write so binder_update_page_range
    doesn't need to hold a mmap_sem as down_write.
    
    Android suffers from mmap_sem contention so let's reduce mmap_sem
    down_write.
    
    Change-Id: I1830d48e21f0ce2aaf09f3949deba0076c3f277b
    Reviewed-by: Martijn Coenen <maco@android.com>
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- drivers/staging/android/binder_alloc.c
+++ drivers/staging/android/binder_alloc.c
@@ -219,7 +219,7 @@ static int binder_update_page_range(struct binder_alloc *alloc, int allocate,
 		mm = get_task_mm(alloc->tsk);
 
 	if (mm) {
-		down_write(&mm->mmap_sem);
+		down_read(&mm->mmap_sem);
 		vma = alloc->vma;
 		if (vma && mm != alloc->vma_vm_mm) {
 			pr_err("%d: vma mm and task mm mismatch\n",
@@ -290,7 +290,7 @@ static int binder_update_page_range(struct binder_alloc *alloc, int allocate,
 		/* vm_insert_page does not seem to increment the refcount */
 	}
 	if (mm) {
-		up_write(&mm->mmap_sem);
+		up_read(&mm->mmap_sem);
 		mmput(mm);
 	}
 	return 0;
