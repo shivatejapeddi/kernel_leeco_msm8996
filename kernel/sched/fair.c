f4b3ba9f2823855d943aa6fddb2e7b8c1bd95e35
DennySPB <dennyspb@gmail.com>
2018-05-03 08:12:44 +0300

    Revert "sched/fair: fix incorrect CPU selection for non latency sensitive tasks"
    This reverts commit 54a28dd7fe39c440937005dc2596f548000d880c.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -6077,6 +6077,7 @@ static int start_cpu(bool prefer_idle)
 static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				   bool prefer_idle)
 {
+	unsigned long best_idle_min_cap_orig = ULONG_MAX;
 	unsigned long min_util = boosted_task_util(p);
 	unsigned long target_capacity = ULONG_MAX;
 	unsigned long min_wake_util = ULONG_MAX;
@@ -6244,13 +6245,6 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 				continue;
 
 			/*
-			 * Favor CPUs with smaller capacity for Non latency
-			 * sensitive tasks.
-			 */
-			if (capacity_orig > target_capacity)
-				continue;
-
-			/*
 			 * Case B) Non latency sensitive tasks on IDLE CPUs.
 			 *
 			 * Find an optimal backup IDLE CPU for non latency
@@ -6277,6 +6271,9 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			if (idle_cpu(i)) {
 				int idle_idx = idle_get_state_idx(cpu_rq(i));
 
+				/* Select idle CPU with lower cap_orig */
+				if (capacity_orig > best_idle_min_cap_orig)
+					continue;
 				/* Favor CPUs that won't end up running at a
 				 * high OPP.
 				 */
@@ -6296,7 +6293,7 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 					continue;
 
 				/* Keep track of best idle CPU */
-				target_capacity = capacity_orig;
+				best_idle_min_cap_orig = capacity_orig;
 				target_idle_max_spare_cap = capacity_orig -
 							    min_capped_util;
 				best_idle_cstate = idle_idx;
@@ -6323,6 +6320,10 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			 * smallest maximum capacity, highest spare maximum
 			 * capacity and highest free cpu utility.
 			 */
+
+			/* Favor CPUs with smaller capacity */
+			if (capacity_orig > target_capacity)
+				continue;
 
 			/* Favor CPUs with maximum spare capacity */
 			if ((capacity_orig == target_capacity) &&
