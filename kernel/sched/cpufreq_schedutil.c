a93b3a463b51f50b3bc900c85a2376d01440a428
shivatejapeddi <shivatejapeddi@gmail.com>
2018-06-22 10:51:15 +0000

    support

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -174,7 +174,7 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 {
 	struct cpufreq_policy *policy = sg_policy->policy;
 	unsigned int freq = arch_scale_freq_invariant() ?
-				policy->cpuinfo.max_freq : policy->cur;
+				policy->max : policy->cur;
 
 	freq = (freq + (freq >> 2)) * util / max;
 
