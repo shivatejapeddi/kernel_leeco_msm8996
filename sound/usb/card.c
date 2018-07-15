6f4047928a0e3de37816d1f0aea601ceeda0c3da
Ícaro Hoff <icarohoff@gmail.com>
2018-07-15 20:40:12 -0300

    sound: usb: list mixer interface separately upon disconnection
    This partially reverts commit 9d263dba25284b31f2544970964decf65b79a2e8.
    
    The current upstream implementation of the commit above misses the new
    mechanism that allows the list of interfaces to be called individually
    from all 'snd_usb_audio_disconnect' consumers.
    
    This should be the correct approach judging other ALSA/USB consumers.
    
    So, ditching the previous upstream patch in favor of the one from CAF,
    which implements the common mixer listing upon disconnection.
    
    Reference commit from CAF: 8bcf6392c1af74eceae468d84a709152b95e0456
    
    Change-Id: I94a3ccb49cefb924c64ed0cd62e267f7538dc91a
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- sound/usb/card.c
+++ sound/usb/card.c
@@ -721,6 +721,7 @@ static void snd_usb_audio_disconnect(struct usb_device *dev,
 				     struct snd_usb_audio *chip)
 {
 	struct snd_card *card;
+	struct usb_mixer_interface *mixer;
 	struct list_head *p;
 	bool was_shutdown;
 
@@ -752,7 +753,8 @@ static void snd_usb_audio_disconnect(struct usb_device *dev,
 		}
 		/* release mixer resources */
 		list_for_each(p, &chip->mixer_list) {
-			snd_usb_mixer_disconnect(p);
+			mixer = list_entry(p, struct usb_mixer_interface, list);
+			snd_usb_mixer_disconnect(mixer);
 		}
 #ifdef CONFIG_VENDOR_LEECO
 		letv_usb_audio.is_letv_usb_audio = false;
