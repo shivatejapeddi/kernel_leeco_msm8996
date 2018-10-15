e81112b5ab5315eb93f74821eaa6181ca0c9594d
shivatejapeddi <shivatejapeddi@gmail.com>
2018-10-15 11:13:36 +0000

    Revert "drivers: cpuidle: lpm-levels: Enable Sleep prediction"
    This reverts commit 3012ba4351242a56f784c49711ead14fdfa1f275.

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -85,9 +85,7 @@ struct lpm_debug {
 
 struct lpm_cluster *lpm_root_node;
 
-#define MAXSAMPLES 5
-
-static bool lpm_prediction = true;
+static bool lpm_prediction;
 module_param_named(lpm_prediction,
 	lpm_prediction, bool, S_IRUGO | S_IWUSR | S_IWGRP);
 
