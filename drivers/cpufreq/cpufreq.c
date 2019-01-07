6bbc10f3643a59447e122a8195185a31ebd51185
shivatejapeddi <shivatejapeddi@gmail.com>
2019-01-07 07:47:22 +0000

    Revert "cpufreq: Kill userspace CPU boosting entirely"
    This reverts commit c89d01e6c5e92e15ab49986cbbc8ea2960b90aaf.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -628,10 +628,12 @@ static ssize_t store_##file_name					\
 	int ret;							\
 	struct cpufreq_policy new_policy;				\
 									\
-	if (&policy->object == &policy->min)				\
-		return count;						\
+	ret = cpufreq_get_policy(&new_policy, policy->cpu);		\
+	if (ret)							\
+		return -EINVAL;						\
 									\
-	memcpy(&new_policy, policy, sizeof(*policy));			\
+	new_policy.min = new_policy.user_policy.min;			\
+	new_policy.max = new_policy.user_policy.max;			\
 									\
 	ret = sscanf(buf, "%u", &new_policy.object);			\
 	if (ret != 1)							\
